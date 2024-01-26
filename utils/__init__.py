4be6097b3b1e624ef1cc607f8cbae7f3fd5ace31
v-sitaocheng <v-sitaocheng@microsoft.com>
2024-01-26 03:45:56 +0000

    修改utils init，吧不需要的工具都删掉了

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .graph_utils import *
-from .utils import *
+from .utils import get_openai_embedding, run_llm, read_jsonl, readjson, savejson, most_similar_string, get_ent_one_hop_rel, entity_search, generate_answer, save_2_jsonl, read_prompt, load_jsonl, load_multiple_jsonl, list_to_string, path_to_string, extract_answer, get_timestamp, dedup_log, similar_search_list
 from .training_utils import *
 from .cloudgpt_aoai_new import get_openai_token
